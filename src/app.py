f1b9ffba730f129992d82607763b7ebfdfb55574
Pablo <ramoswilkinspj28@gmail.com>
2023-08-08 21:13:30 -0500

    adaptando el codigo a un registro de bódega

--- src/app.py
+++ src/app.py
@@ -14,5 +14,13 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
+
+
+#
