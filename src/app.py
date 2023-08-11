3166820bb3e743e48270ffc64ad4895cdd8cc496
Pablo <ramoswilkinspj28@gmail.com>
2023-08-10 21:19:53 -0500

    cambios minimos

--- src/app.py
+++ src/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, SubmitField
 #import database as db
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
@@ -13,14 +15,39 @@ from productos import productos
 productos = productos
 
 # Rutas de la aplicación 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        id_ = request.form['idProducto']
+        nombre = request.form['nombreProducto']
+        proveedor = request.form['proveedorProducto']
+        categoria = request.form['categoriaProducto']
+        lote = request.form['loteProducto']
+        cantidad = request.form['cantidadProducto']
+        descripcion = request.form['descripcionProducto']
+
+        producto= {'id':id_, 'nombre': nombre, 'proveedor':proveedor, 'categoria':categoria, 'lote':lote, 'cantidad':cantidad, 'descripcion':descripcion}
+
+    
     return render_template('index.html')
 
 
 @app.route('/registro')
 def registro():
     return render_template('registro.html', productos=productos)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Nombre de Usuario: ")
+    password = PasswordField(" Contraseña: ")
+    email = EmailField("Correo Electrónico: ")
+    repeatPassword = PasswordField("Confirma la Contraseña: ")
+
+
+@app.route('/register')
+def register():
+    form = RegisterForm
+    return render_template('auth/register.html', form=form)
 
 
 if __name__ == '__main__':
