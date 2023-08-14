6e730ee49fc42b1792c4ca080cd245c0601197be
Pablo <ramoswilkinspj28@gmail.com>
2023-08-13 21:15:55 -0500

    signup ready

--- src/app.py
+++ src/app.py
@@ -2,13 +2,28 @@ import os
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField, SubmitField
-#import database as db
+from wtforms.validators import DataRequired, Length
+
+from database import SessionLocal
+from sqlalchemy.orm import Session
+
 
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir, 'src', 'templates')
 
 
 app = Flask(__name__, template_folder=template_dir)
+app.config.from_mapping(
+    SECRET_KEY = 'dev'
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 from productos import productos
@@ -26,8 +41,8 @@ def home():
         cantidad = request.form['cantidadProducto']
         descripcion = request.form['descripcionProducto']
 
-        producto= {'id':id_, 'nombre': nombre, 'proveedor':proveedor, 'categoria':categoria, 'lote':lote, 'cantidad':cantidad, 'descripcion':descripcion}
-
+        producto= [id_, nombre, proveedor, categoria, lote, cantidad, descripcion]
+       # db.add(producto)
     
     return render_template('index.html')
 
@@ -37,16 +52,25 @@ def registro():
     return render_template('registro.html', productos=productos)
 
 
-class RegisterForm(FlaskForm):
-    username = StringField("Nombre de Usuario: ")
-    password = PasswordField(" Contraseña: ")
-    email = EmailField("Correo Electrónico: ")
-    repeatPassword = PasswordField("Confirma la Contraseña: ")
+class SignUpForm(FlaskForm):
+    username = StringField("Nombre de Usuario: ", validators=[DataRequired(), Length(min=4, max=12)])
+    email = EmailField("Correo Electrónico: ", validators=[DataRequired()])
+    password = PasswordField(" Contraseña: ", validators=[DataRequired(), Length(min=4, max=25)])
+    password2 = PasswordField("Confirma la Contraseña: ", validators=[DataRequired(), Length(min=4, max=25)])
+    submit = SubmitField("Registrar")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm
+    form = SignUpForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        password2 = form.password2.data
+
+        return f"{username}, {email}"
+
     return render_template('auth/register.html', form=form)
 
 
--- src/database.py
+++ src/database.py
@@ -1,8 +1,11 @@
-import mysql.connector
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-database = mysql.connector.connect(
-    host='sql514.main-hosting.eu',
-    user='',
-    password='',
-    database=''
-)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./registros_db.db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
