d037717906f0397d889ac50c7e27fb60d6a295e6
Pablo <ramoswilkinspj28@gmail.com>
2023-08-17 18:39:05 -0500

    cagada

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
-
 auth_bp = Blueprint('auth', __name__)
 
-
-from . import views
+from app.auth import routes
--- run.py
+++ run.py
@@ -1,10 +1,6 @@
-from flask import Flask
-from app.auth import auth_bp
+from app import create_app
 
+app = create_app()
 
-app = Flask(__name__, template_folder='templates')
-app.register_blueprint(auth_bp, url_prefix='/auth')
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
